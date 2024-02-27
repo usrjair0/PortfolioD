fd21d4a126a26c8af0e3331bc19161f4017bbb97
usrjair0 <juniorsilvasobrinho1999@gmail.com>
2024-02-26 21:42:56 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.scss'
+import Navbar from './Componentes/Navbar/Navbar'
+
+function App() {
+  return (
+    <>
+      <Navbar/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Componentes/Navbar/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import github from "../../assets/github.svg";
+import linkedin from "../../assets/linkedin.svg";
+import "./index.scss";
+
+function Navbar() {
+  return (
+    <header className='header'>
+      <ul className='header__names'>
+        <li>Projetos</li>
+        <li>Tecnologias</li>
+        <li>Sobre mim</li>
+      </ul>
+      <ul className='header__images'>
+        <li><img src={github} alt="github icon"/></li>
+        <li><img src={linkedin} alt="github icon"/></li>
+      </ul>
+    </header>
+  )
+}
+
+export default Navbar
