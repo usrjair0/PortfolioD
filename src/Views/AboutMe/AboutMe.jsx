b6cdaba91b174cafda434a51da46ad9eb5ac85a0
usrjair0 <juniorsilvasobrinho1999@gmail.com>
2024-02-28 23:28:51 -0300

    finalizado aboutmept1

--- src/Views/AboutMe/AboutMe.jsx
+++ src/Views/AboutMe/AboutMe.jsx
@@ -3,7 +3,7 @@ import "./index.scss";
 
 const AboutMe = () => {
     const [progressWidth, setProgressWidth] = useState('3%');
-    const [activeYear, setActiveYear] = useState('ano2023');
+    const [activeYear, setActiveYear] = useState('ano2022');
 
     const handleStepClick = (width, year) => {
         setProgressWidth(width);
@@ -17,10 +17,13 @@ const AboutMe = () => {
             <div className="process-wrapper">
                 <div id="progress-bar-container">
                     <ul>
-                        <li className={`step step01 ${activeYear === 'ano2023' ? 'active' : ''}`} onClick={() => handleStepClick('3%', 'ano2023')}>
+                        <li className={`step step01 ${activeYear === 'ano2022' ? 'active' : ''}`} onClick={() => handleStepClick('3%', 'ano2022')}>
+                            <div className="step-inner">2022</div>
+                        </li>
+                        <li className={`step step02 ${activeYear === 'ano2023' ? 'active' : ''}`} onClick={() => handleStepClick('25%', 'ano2023')}>
                             <div className="step-inner">2023</div>
                         </li>
-                        <li className={`step step02 ${activeYear === 'ano2024' ? 'active' : ''}`} onClick={() => handleStepClick('25%', 'ano2024')}>
+                        <li className={`step step03 ${activeYear === 'ano2024' ? 'active' : ''}`} onClick={() => handleStepClick('50%', 'ano2024')}>
                             <div className="step-inner">2024</div>
                         </li>
                     </ul>
@@ -30,13 +33,19 @@ const AboutMe = () => {
                 </div>
 
                 <div id="progress-content-section">
+                    <div className={`section-content ${activeYear === 'ano2022' ? 'active' : ''}`}>
+                        <h2>2022</h2>
+                        <p>
+                            Cursei engenharia elétrica até esse ano que foi durante o periodo da pandemia. Tive 
+                            meu primeiro contato com a programação com uma matéria chamada programação avançada. Após 
+                            isso eu comecei a gostar bastante da área e me identificar ao ponto de ter trocar o curso
+                            por engenharia de software e evoluir na área de desenvolvimento web.
+                        </p>
+                    </div>
                     <div className={`section-content ${activeYear === 'ano2023' ? 'active' : ''}`}>
                         <h2>2023</h2>
                         <p>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                            Dolorem consequatur repellendus pariatur, voluptatibus est sed reiciendis
-                            , totam vel, expedita recusandae odio numquam harum. Id impedit libero
-                            ipsam tempore laborum facere.
+                            No ano de 2023 comecei a estudar com materiais na internet e cursos gratuitos.
                         </p>
                     </div>
 
